4611f10f894377f5e6154c875b6088441f00e61c
Erik Hellenberg <ehelle@kth.se>
2018-04-13 15:13:38 +0200

    Skapar ett nytt game window
    Programmet skapar ett tomt game window som kan avslutas genom att klicka på krysset.

--- Client/main.c
+++ Client/main.c
@@ -1,67 +1,101 @@
-/* Stephen's Tutorials (http://stephenmeier.net/)
-gcc main.c -o game.exe -I./include -L./lib -lSDL2main -lSDL2
-running on gcc 4.8.1, SDL 2.0.1
-*/
-
-//----------------------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
 
-//----------------------------------------------------------------------
 #define SDL_MAIN_HANDLED
 #include "SDL.h"
+
+#define SCREEN_w 640
+#define SCREEN_H 480
+#define SCREEN_SCALE 1
+#define SCREEN_NAME "Prototype"
 
 //----------------------------------------------------------------------
 void game_init(void);
 void game_quit(void);
 
-static struct {
+struct {
 	// define "attributes"
 	SDL_bool running;
+	struct {
+		unsigned int w;
+		unsigned int h;
+		const char* name;
+		SDL_Window* window;
+		SDL_Renderer* renderer;
+	} screen;
 
 	// define "methods"
 	void(*init)(void);
 	void(*quit)(void);
 } Game = {
 	SDL_FALSE,
-	game_init,
-	game_quit
+{
+	SCREEN_SCALE*SCREEN_w,
+	SCREEN_SCALE*SCREEN_H,
+	SCREEN_NAME,
+	NULL,
+	NULL
+},
+game_init,
+game_quit
 };
 
 //----------------------------------------------------------------------
 void game_init(void) {
-	printf("game_init()\n");
-
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		printf("SDL error -> %s\n", SDL_GetError());
+		printf("SDL error -> %sn", SDL_GetError());
 		exit(1);
 	}
+
+	unsigned int w = Game.screen.w;
+	unsigned int h = Game.screen.h;
+	const char* name = Game.screen.name;
+
+	Game.screen.window = SDL_CreateWindow(
+		name,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		w, h, 0
+	);
+	Game.screen.renderer = SDL_CreateRenderer(
+		Game.screen.window, -1,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+	);
 
 	Game.running = SDL_TRUE;
 }
 
 //----------------------------------------------------------------------
 void game_quit(void) {
-	printf("game_quit()\n");
+	SDL_DestroyRenderer(Game.screen.renderer);
+	SDL_DestroyWindow(Game.screen.window);
+
+	Game.screen.window = NULL;
+	Game.screen.renderer = NULL;
+
 	SDL_Quit();
-	Game.running = SDL_FALSE;
 }
 
 //----------------------------------------------------------------------
 int main(int argc, char* argv[]) {
 
-	printf("running = %d\n", Game.running);
-
 	Game.init();
 
-	printf("running = %d\n", Game.running);
-
+	SDL_Event event;
 	while (Game.running) {
-		SDL_Delay(5000);
-		Game.quit();
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT: {
+				Game.running = SDL_FALSE;
+			} break;
+			}
+		}
+
+		SDL_RenderClear(Game.screen.renderer);
+		SDL_RenderPresent(Game.screen.renderer);
 	}
 
-	printf("running = %d\n", Game.running);
+	Game.quit();
 
 	return 0;
 }
