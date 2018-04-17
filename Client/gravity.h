e20e21feeddc7616f0bad3c9e92b5129ab0e30a0
Mimmi <mimmi@kth.se>
2018-04-17 11:37:44 +0200

    change in jump()

--- Client/gravity.h
+++ Client/gravity.h
@@ -4,7 +4,7 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
-void gravity(SDL_Rect *player, SDL_Rect *weapon) {
+void gravity(SDL_Rect *player, SDL_Rect *weapon) {   
 	if (player->y < 480 && player->y > 0) {
 		player->y += player->y * 0.025;
 		weapon->y = player->y + 50;
@@ -19,20 +19,24 @@ void gravity(SDL_Rect *player, SDL_Rect *weapon) {
 	}
 }
 
-void jump(SDL_Rect *player, SDL_Rect *weapon, int *isJumping, int *jumpTime, int *doJump) {
-	if (*doJump == 1) {
-		if (!*isJumping) {
-			*jumpTime = 10;
+void jump(SDL_Rect *player, SDL_Rect *weapon, int *isJumping, int *jumpTime, int *doJump) {		
+	
+	if ((*doJump == 1))				
+	{			
+		if (!*isJumping) {			
+			*jumpTime = 10;			
 		}
-		if (*jumpTime > 0) {
+		if (*jumpTime > 0) {        
+			*isJumping = 1;			
 			player->y -= 20;
 			weapon->y = player->y + 50;
-			--*jumpTime;
-			*isJumping = 1;
+			--(*jumpTime);
 		}
-		else {
-			*isJumping = 0;
-			*doJump = 0;
+		if (*jumpTime <= 0) {
+			if (player->y == 480) {
+				*doJump = 0;
+				*isJumping = 0;
+			}
 		}
 	}
 }
