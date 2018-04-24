e91ce8b9643baa0e9f48836987e752851c08fe85
Alfred <alfberg@kth.se>
2018-04-24 16:17:24 +0200

    Create projectile with space

---
+++ Client/projectile.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL.h>
+#include <SDL_image.h>
+
+#define MAXPROJECTILES 1
+#define MAXPROJECTILEOBJECTS 100
+
+struct projectile {
+	int id;
+	int dmg;
+	int speed;
+	int projectilesFired;
+	int w;
+	int h;
+	SDL_Surface *Image;
+	SDL_Texture *Texture;
+	SDL_Rect rect[MAXPROJECTILEOBJECTS];
+	int direction[MAXPROJECTILEOBJECTS];
+};
+typedef struct projectile Projectile;
+
